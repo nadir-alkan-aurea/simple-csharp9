56d6079cb156f90af4d0bbd50609bcf562f4465c
Nadir Alkan <nadir.alkan@aurea.com>
2019-10-24 10:05:39 +0300

    First

---
+++ directory1/directory2/ALongClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simple_csharp.directory1.directory2
+{
+    class ALongClass
+    {
+        public void y()
+        {
+            int abc = 10;
+            int cde = abc * 5;
+            abc = cde + 7;
+        }
+
+        public void z()
+        {
+            int ab = 10;
+            int cd = ab * 5;
+            ab = cd + 7;
+        }
+
+        public void aLongMethod()
+        {
+            int xy = 10;
+            int yz = xy * 5;
+            xy = yz + 7;
+        }
+    }
+}
